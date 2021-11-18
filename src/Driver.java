0e457ad87ba22e1edeb617cdabad82ed0c73d15b
Abdallahhhassan <abdullahhassan436@gmail.com>
2021-11-18 18:45:00 +0200

    Driver class

--- src/Driver.java
+++ src/Driver.java
@@ -3,12 +3,15 @@ import java.util.List;
 
 public class Driver extends SystemUser{
     int Balance;
+    String nationalID , License;
     List<String> FavArea = new ArrayList<String>();
-    Driver(String username , String password , String Mobileno , String Email){
+    Driver(String username , String password , String Mobileno , String Email, String nationalid , String license){
         this.Username = username;
         this.Password = password;
         this.MobileNumber = Mobileno;
         this.Email = Email;
+        this.nationalID = nationalid;
+        this.License = license;
     }
     public void Login(String username, String password) {}
     public void AddFavoriteArea(String favouritearea)
