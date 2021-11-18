dfbbde64e690687a8041290f7b149170cc2bfca7
MohamedTarek <mohamedtarek20196046@gmail.com>
2021-11-18 19:01:52 +0200

    UserManger

---
+++ src/UserManger.java
@@ -0,0 +1,8 @@
+public class UserManger {
+    User u;
+    Driver d;
+    void RegisterUser() {}
+    void RegisterDriver() {}
+    void getPendingRegisterations() {}
+    void getverfication() {}
+}
