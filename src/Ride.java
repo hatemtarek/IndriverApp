1793e0834f3f0eb5d67093968ef6955bc747b32b
OmarYasser21 <omaryasser21780@gmail.com>
2021-11-18 20:17:58 +0200

    Ride

---
+++ src/Ride.java
@@ -0,0 +1,25 @@
+public class Ride {
+    String SourceArea;
+    String Destination;
+    String Title;
+    String Description;
+    String TimeToLeave;
+    int Cost;
+
+    public void RequestRide(String sourceArea,String destination)
+    {
+
+    }
+    public void AnnounceNewRide(String SourceArea,String Destination,String Title,String Description,String TimeToLeave,int Cost)
+    {
+
+    }
+    public void EndRide()
+    {
+
+    }
+    public void RateRide()
+    {
+
+    }
+}
